385603c22a35aa241ecc91756a219d554af42329
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-05-18 18:32:20 +0530

    Created prototypal chain

--- prototype.js
+++ prototype.js
@@ -16,18 +16,34 @@ They are called with the new keyword to create instances.*/
 // person1.sayHello();
 
 
+// const p1 = {
+//     fname: "vijay",
+//     lname: "jadon",
+//     getFullName() {
+//         return (`${this.fname} ${this.lname}`);
+//     }
+// }
+
+// const p2 = Object.create(p1);
+// // p2.fname = "ajay";
+// p2.__proto__.fname = "ajay";
+
+// console.log(p2.getFullName());
+// console.log(p1.getFullName());
+// console.log(p2.__proto__);
+
+
 const p1 = {
-    fname: "vijay",
-    lname: "jadon",
-    getFullName() {
-        return (`${this.fname} ${this.lname}`);
-    }
+    xp1: "I am inside p1"
+}
+const p2 = {
+    xp2: "I am inside p2",
+    __proto__:p1,
+}
+const p3 = {
+    xp3: "I am inside p3",
+    __proto__:p2
 }
 
-const p2 = Object.create(p1);
-// p2.fname = "ajay";
-p2.__proto__.fname = "ajay";
+console.log(p3.xp1)
 
-console.log(p2.getFullName());
-console.log(p1.getFullName());
-console.log(p2.__proto__);
