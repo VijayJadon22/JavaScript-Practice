b0a6c947ea27ad49805a73dfa6451e396784ed56
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-05-18 16:52:04 +0530

    Implemnted prototypal inheritance

--- prototype.js
+++ prototype.js
@@ -4,13 +4,30 @@
 They start with an uppercase letter by convention (e.g., Person instead of person).
 They use this to assign properties to the newly created object.
 They are called with the new keyword to create instances.*/
-function Person(name) {
-    this.name = name;
+// function Person(name) {
+//     this.name = name;
+// }
+
+// Person.prototype.sayHello = function() {
+//     console.log(`Hi USer: ${this.name}`);
+// }
+
+// const person1 = new Person("swati");
+// person1.sayHello();
+
+
+const p1 = {
+    fname: "vijay",
+    lname: "jadon",
+    getFullName() {
+        return (`${this.fname} ${this.lname}`);
+    }
 }
 
-Person.prototype.sayHello = function() {
-    console.log(`Hi USer: ${this.name}`);
-}
+const p2 = Object.create(p1);
+// p2.fname = "ajay";
+p2.__proto__.fname = "ajay";
 
-const person1 = new Person("swati");
-person1.sayHello();
+console.log(p2.getFullName());
+console.log(p1.getFullName());
+console.log(p2.__proto__);
